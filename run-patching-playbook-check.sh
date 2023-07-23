667f1e0480eda589c7d1b3dabadc1495bca59688
user1 <Joey Allen>
2023-07-23 09:18:30 +0100

    Initial Commit

--- run-patching-playbook-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ANSIBLE_PLAYBOOK=`which ansible-playbook`
-cd /home/user1/CodeCommit/ansible_repo/Ansible_Updates/Linux_Updates/
-$ANSIBLE_PLAYBOOK update-by-group.yml --limit local:servers:kvm -C -v
-
-exit 0
