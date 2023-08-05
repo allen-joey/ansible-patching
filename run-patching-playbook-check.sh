a9405b82b904a5931bd5adefb1bfdad107c671e9
user1 <Joey Allen>
2023-08-05 16:40:04 +0100

    Initial Commit

--- run-patching-playbook-check.sh
+++ run-patching-playbook-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ANSIBLE_PLAYBOOK=`which ansible-playbook`
-$ANSIBLE_PLAYBOOK update-by-group.yml --limit local:servers:kvm:kvm_test -C -v
+$ANSIBLE_PLAYBOOK update-by-group.yml --limit your:inventory:groups -C -v
 
 exit 0
--- run-patching-playbook.sh
+++ run-patching-playbook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ANSIBLE_PLAYBOOK=`which ansible-playbook`
-$ANSIBLE_PLAYBOOK update-by-group.yml --limit local:servers:kvm:kvm_test
+$ANSIBLE_PLAYBOOK update-by-group.yml --limit your:inventory:groups
 
 exit 0
